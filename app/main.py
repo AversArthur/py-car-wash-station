100e55179335c5e9adb1ca01d6e355e9ab7e901d
Arthur <avers.target@gmail.com>
2024-08-14 15:27:39 +0200

    Solution v2 'flake8 suggestions edit'

--- app/main.py
+++ app/main.py
@@ -1,18 +1,28 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str):
+    def __init__(self,
+                 comfort_class: int,
+                 clean_mark: int,
+                 brand: str
+                 ) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float, clean_power: int, average_rating: float, count_of_ratings: int):
+    def __init__(
+            self,
+            distance_from_city_center: float,
+            clean_power: int,
+            average_rating: float,
+            count_of_ratings: int,
+    ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = round(average_rating, 1)
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars:list):
+    def serve_cars(self, cars: list) -> float:
         income = 0.0
         for car in cars:
             if car.clean_mark < self.clean_power:
@@ -21,20 +31,21 @@ class CarWashStation:
                 self.wash_single_car(car)
         return round(income, 1)
 
-    def calculate_washing_price(self, car: Car):
+    def calculate_washing_price(self, car: Car) -> float:
         if car.clean_mark < self.clean_power:
             gross_profit = (car.comfort_class
                             * (self.clean_power - car.clean_mark)
-                            * self.average_rating)
+                            * self.average_rating
+                            )
             return round(gross_profit / self.distance_from_city_center, 1)
         else:
             return 0.0
 
-    def wash_single_car(self, car: Car):
+    def wash_single_car(self, car: Car) -> None:
         if car.clean_mark < self.clean_power:
             car.clean_mark = self.clean_power
 
-    def rate_service(self, mark:int):
+    def rate_service(self, mark: int) -> None:
         total_rating = (self.average_rating * self.count_of_ratings) + mark
         self.count_of_ratings += 1
         self.average_rating = round(total_rating / self.count_of_ratings, 1)
