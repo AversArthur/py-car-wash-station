d42354ecb27ce01b29de8ea4d137ea328c89f146
Arthur <avers.target@gmail.com>
2024-08-14 15:59:01 +0200

    Solution v2 'required edits

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 class Car:
-    def __init__(self,
-                 comfort_class: int,
+    def __init__(self, comfort_class: int,
                  clean_mark: int,
                  brand: str
                  ) -> None:
@@ -11,18 +10,18 @@ class Car:
 
 class CarWashStation:
     def __init__(
-            self,
-            distance_from_city_center: float,
-            clean_power: int,
-            average_rating: float,
-            count_of_ratings: int,
+        self,
+        distance_from_city_center: float,
+        clean_power: int,
+        average_rating: float,
+        count_of_ratings: int,
     ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = round(average_rating, 1)
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: list) -> float:
+    def serve_cars(self, cars: list[Car]) -> float:
         income = 0.0
         for car in cars:
             if car.clean_mark < self.clean_power:
@@ -33,13 +32,12 @@ class CarWashStation:
 
     def calculate_washing_price(self, car: Car) -> float:
         if car.clean_mark < self.clean_power:
-            gross_profit = (car.comfort_class
-                            * (self.clean_power - car.clean_mark)
-                            * self.average_rating
-                            )
+            gross_profit = (
+                car.comfort_class
+                * (self.clean_power - car.clean_mark)
+                * self.average_rating
+            )
             return round(gross_profit / self.distance_from_city_center, 1)
-        else:
-            return 0.0
 
     def wash_single_car(self, car: Car) -> None:
         if car.clean_mark < self.clean_power:
